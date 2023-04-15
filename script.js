788253d16b79b4a161a1324045eea992cbc8facf
nikhil-rawat02 <117645790+nikhil-rawat02@users.noreply.github.com>
2023-04-15 23:20:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let element = document.getElementById("browser-info");
+let browserInfo = navigator.appName;
+let version = navigator.appVersion;
+let info =  "You are using " + browserInfo + " version " + version;
+// console.log(info);
+element.innerHTML = info;
